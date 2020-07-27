3e6422fb0d839731d44a3675a01272ed4067dfd1
guomz <1258746567@qq.com>
2020-07-27 10:10:00 +0800

    判断子串

---
+++ src/com/guomz/OfferTest/is_subsequence/Main.java
@@ -0,0 +1,51 @@
+package com.guomz.OfferTest.is_subsequence;
+
+/**
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ *
+ * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+ *
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ *
+ * 示例 1:
+ * s = "abc", t = "ahbgdc"
+ *
+ * 返回 true.
+ *
+ * 示例 2:
+ * s = "axc", t = "ahbgdc"
+ *
+ * 返回 false.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/is-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc"));
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        if (t.length() < s.length()){
+            return false;
+        }
+
+        if (s.length() == 0){
+            return true;
+        }
+
+        int i = 0;
+        int j = 0;
+        for (;j < t.length(); j ++){
+            if (s.charAt(i) == t.charAt(j)){
+                i ++;
+            }
+            if (i >= s.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+}
