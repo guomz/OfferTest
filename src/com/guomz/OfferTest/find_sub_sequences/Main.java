7cc78ce3a8938ad6d0ab43271c2d26c1366a58e6
guomz <1258746567@qq.com>
2020-08-26 09:09:49 +0800

    添加注释

--- src/com/guomz/OfferTest/find_sub_sequences/Main.java
+++ src/com/guomz/OfferTest/find_sub_sequences/Main.java
@@ -2,6 +2,23 @@ package com.guomz.OfferTest.find_sub_sequences;
 
 import java.util.*;
 
+/**
+ * 给定一个整型数组, 你的任务是找到所有该数组的递增子序列，递增子序列的长度至少是2。
+ *
+ * 示例:
+ *
+ * 输入: [4, 6, 7, 7]
+ * 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+ * 说明:
+ *
+ * 给定数组的长度不会超过15。
+ * 数组中的整数范围是 [-100,100]。
+ * 给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/increasing-subsequences
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -20,6 +37,11 @@ public class Main {
 //        System.out.println(list1.containsAll(list2));
     }
 
+    /**
+     * 一个长为n的串有2的n次方减一种组合，思路为使用二进制01串来表示哪个字符被选中
+     * @param nums
+     * @return
+     */
     public static List<List<Integer>> findSubsequences(int[] nums) {
 
         if (nums == null || nums.length < 2){
